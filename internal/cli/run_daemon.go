34495414da6c22b2e9f717262309229d5ddbcfaa
csk6124 <csk6124@gmail.com>
2026-03-16 13:15:26 +0900

    chore: bump version to v0.2.1

--- internal/cli/run_daemon.go
+++ internal/cli/run_daemon.go
@@ -21,7 +21,7 @@ import (
 	"github.com/nlook-service/nlook-router/internal/ws"
 )
 
-const version = "0.2.0"
+const version = "0.2.1"
 
 // RunDaemon starts the local HTTP server, heartbeat loop, WebSocket client, and SSH proxy.
 func RunDaemon(cfg *config.Config) error {
